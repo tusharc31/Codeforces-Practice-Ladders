e7e91549ab1afec4e69d3963788a04a889678932
Tushar Choudhary <hitusharonline31@gmail.com>
2021-08-04 07:22:17 +0530

    Added subprocess to open problem link

--- script.py
+++ script.py
@@ -8,7 +8,7 @@ min_rating = 1800
 max_rating = 1900
 
 
-# Returns list of contest IDs from which questions are to be picked
+# Returns list of contest IDs from which problems are to be picked
 def fetch_contests():
     data = requests.get("https://codeforces.com/api/contest.list").json()
     contests = []
@@ -20,40 +20,40 @@ def fetch_contests():
     return contests
 
 
-# Returns desirable questions in sorted order
-def fetch_total_questions(contests):
+# Returns desirable problems in sorted order
+def fetch_total_problems(contests):
     data = requests.get(
         "https://codeforces.com/api/problemset.problems").json()
-    total_questions = []
+    total_problems = []
     for i in data['result']['problems']:
         if i['contestId'] in contests:
             try:
                 if i['rating'] >= min_rating and i['rating'] <= max_rating:
-                    total_questions.append(i)
+                    total_problems.append(i)
             except:
                 pass
-    total_questions.sort(key=lambda x: x["rating"])
-    return total_questions
+    total_problems.sort(key=lambda x: x["rating"])
+    return total_problems
 
 
-# Return questions already solved by the user
+# Return problems already solved by the user
 def fetch_user_solves():
-    questions_finished = []
+    problems_finished = []
     data = requests.get(
         "https://codeforces.com/api/user.status?handle="+handle).json()
     for i in data['result']:
         if i['verdict'] == "OK":
-            questions_finished.append(i['problem'])
-    return questions_finished
+            problems_finished.append(i['problem'])
+    return problems_finished
 
 
-# Search and print the most relevant question
-def search(total_questions, questions_finished):
+# Search and print the most relevant problem
+def search(total_problems, problems_finished):
     found_status = False
     todo = None
     done = 0
-    for i in total_questions:
-        if i in questions_finished:
+    for i in total_problems:
+        if i in problems_finished:
             done += 1
         elif found_status is False:
             todo = i
@@ -63,11 +63,11 @@ def search(total_questions, questions_finished):
     print("\nSuggested Problem: "+todo['name'])
     print("Link: "+url)
     print("Current Progress: "+str(done)+'/' +
-          str(len(total_questions))+' Questions\n')
+          str(len(total_problems))+' Problems\n')
     return url
 
 
-# Launches cpp file with template code and opens the question found in browser
+# Launches cpp file with template code and opens the problem found in browser
 def setup(url):
     # uncomment if you wish to open 1.cpp in vs code
     # subprocess.call(["code", "1.cpp"])
@@ -76,7 +76,7 @@ def setup(url):
 
 # Calling relevent functions
 _contests = fetch_contests()
-_total_questions = fetch_total_questions(_contests)
-_questions_finished = fetch_user_solves()
-_url = search(_total_questions, _questions_finished)
+_total_problems = fetch_total_problems(_contests)
+_problems_finished = fetch_user_solves()
+_url = search(_total_problems, _problems_finished)
 setup(_url)
